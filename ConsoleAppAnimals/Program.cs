a400c85976c0d7fb349310bd4d3715deaa9e32ad
Enzo VOISIN <enzovoisin1@outlook.fr>
2023-03-11 18:47:01 +0100

    ISP

--- ConsoleAppAnimals/Program.cs
+++ ConsoleAppAnimals/Program.cs
@@ -1,6 +1,8 @@
 ﻿using ConsoleAppAnimals.Models;
 using ConsoleAppAnimals.Models.Animals;
 using ConsoleAppAnimals.Models.Vehicules;
+using System;
+using System.Data.SqlClient;
 
 namespace ConsoleAppAnimals
 {
@@ -11,26 +13,31 @@ namespace ConsoleAppAnimals
             Griffin griffin = new Griffin("Griffin", "Rare", 10);
             Dragons dragons = new Dragons("Dragon", "Rare", 12);
 
-            Dog max = new Dog("Max", "Commun", 15);
-            Dog rocky = new Dog("Rocky", "Commun", 17);
-            Dog sonny = new Dog("Sonny", "Commun", 8);
-            Dog snow = new Dog("Snow", "Commun", 6);
-            Dog sun = new Dog("Sun", "Commun", 20);
+            Bird bird = new Bird("Pioupiou2", "Légendaire");
 
-            List<Animal> animals = new List<Animal> { griffin, dragons, rocky, max, sonny, snow, sun };
+            Eagle eagle = new Eagle("Eagle", "Légendaire");
 
-            IEnumerable<Animal> onlyRare = animals.Where(or => or.Rarity == "Rare").ToList();
-            IEnumerable<Animal> onlyChild = animals.Where(or => or.Old >= 10 && or.Old <= 15).ToList();
 
-            foreach(Animal animalRare in onlyRare)
-            {
-                Console.WriteLine($"Name {animalRare.Name}, Rarity : {animalRare.Rarity}");
-            }
+            //Dog max = new Dog("Max", "Commun", 15);
+            //Dog rocky = new Dog("Rocky", "Commun", 17);
+            //Dog sonny = new Dog("Sonny", "Commun", 8);
+            //Dog snow = new Dog("Snow", "Commun", 6);
+            //Dog sun = new Dog("Sun", "Commun", 20);
 
-            foreach(Animal animalChild in onlyChild)
-            {
-                Console.WriteLine($"Name {animalChild.Name}, Old : {animalChild.Old}");
-            }
+            //List<Animal> animals = new List<Animal> { griffin, dragons, rocky, max, sonny, snow, sun };
+
+            //IEnumerable<Animal> onlyRare = animals.Where(or => or.Rarity == "Rare").ToList();
+            //IEnumerable<Animal> onlyChild = animals.Where(or => or.Old >= 10 && or.Old <= 15).ToList();
+
+            //foreach (Animal animalRare in onlyRare)
+            //{
+            //    Console.WriteLine($"Name {animalRare.Name}, Rarity : {animalRare.Rarity}");
+            //}
+
+            //foreach (Animal animalChild in onlyChild)
+            //{
+            //    Console.WriteLine($"Name {animalChild.Name}, Old : {animalChild.Old}");
+            //}
 
 
         }
